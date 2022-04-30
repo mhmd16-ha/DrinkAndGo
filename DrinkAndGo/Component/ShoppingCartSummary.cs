082ea9f773bc08f8dee3366cadc1f48a55e76ecf
mhmd16-ha <https://github.com/mhmd16-ha/Todo_List.git>
2022-04-30 03:05:35 +0200

    Add Layout And View Component

---
+++ DrinkAndGo/Component/ShoppingCartSummary.cs
@@ -0,0 +1,32 @@
+﻿using DrinkAndGo.Data.Models;
+using DrinkAndGo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DrinkAndGo.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShopingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShopingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart ??
+                throw new ArgumentNullException(nameof(shoppingCart));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShopingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
